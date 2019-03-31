83b0c34e46689ca0a304477f44f8ecf52f75f41e
Victoria Som de Cerff <vsomdec@Victorias-MacBook-Air.local>
2019-03-31 10:00:34 -0400

    Factorial example

---
+++ compile.sh
@@ -0,0 +1,1 @@
+g++ -std=c++11 -Wall -I/Users/vsomdec/workspace/Software_Engineering/project/code/Catch2-2.7.0/build/install/include/ -o test.out test.cpp program.cpp
---
+++ src/test.cpp
@@ -0,0 +1,23 @@
+// 010-TestCase.cpp
+
+// Let Catch provide main():
+#define CATCH_CONFIG_MAIN
+
+#include <catch2/catch.hpp>
+#include "program.h"
+
+TEST_CASE( "Factorial of 0 is 1 (fail)", "[single-file]" ) {
+    REQUIRE( Factorial(0) == 1 );
+}
+
+TEST_CASE( "Factorials of 1 and higher are computed (pass)", "[single-file]" ) {
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
+}
+
+// Compile & run:
+// - g++ -std=c++11 -Wall -I$(CATCH_SINGLE_INCLUDE) -o 010-TestCase 010-TestCase.cpp && 010-TestCase --success
+// - cl -EHsc -I%CATCH_SINGLE_INCLUDE% 010-TestCase.cpp && 010-TestCase --success
+
