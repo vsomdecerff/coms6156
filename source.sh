a94325d6344d457db7bcbc028975c6c38fd691ba
Victoria Som de Cerff <vsomdec@Victorias-MacBook-Air.local>
2019-04-29 23:14:32 -0400

    source

---
+++ source.sh
@@ -0,0 +1,2 @@
+export MPI_DIR=/Users/vsomdec/workspace/Software_Engineering/project/code/openmpi-4.0.0/install/
+export CATCH_DIR=/Users/vsomdec/workspace/Software_Engineering/project/code/Catch2-2.7.0/single_include/
